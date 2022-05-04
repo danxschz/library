ef72515cafb885cb7b0f0cd86c806097c6945933
Daniel Sanchez <danxschz@gmail.com>
2022-05-04 02:56:19 -0400

    Organize script and improve functions

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+// User library
 let myLibrary = [];
 
+// Book constructor
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -7,16 +9,22 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
+// Main functions
+
+// Add book to library
 const title = document.querySelector('#title');
 const author = document.querySelector('#author');
 const pages = document.querySelector('#pages');
 const read = document.querySelector('#read');
+
 function addBookToLibrary() {
+  // If any value is empty, do nothing
   if (title.value === "" || author.value === "" || pages.value === "") return;
   newBook = new Book(title.value, author.value, pages.value, read.checked);
   myLibrary.push(newBook);
 }
 
+// Reset book-form inputs
 function resetInputs() {
   title.value = "";
   author.value = "";
@@ -24,68 +32,106 @@ function resetInputs() {
   read.checked = false;
 }
 
+// Display books
+const booksContainer = document.querySelector('.books');
+
 function displayBooks() {
-  const booksContainer = document.querySelector('.books');
   let bookContainer;
+
+  let bookInfo;
   let bookTitle;
   let bookAuthor;
   let bookPages;
   let bookRead;
+  let readStr;
+
+  let bookIcons;
+  let readIcon;
+  let deleteIcon;
 
   for (book of myLibrary) {
     bookContainer = document.createElement('div');
-    bookContainer.classList.add('book');
+    bookContainer.classList.add('books__book');
+
+    bookInfo = document.createElement('div');
+    bookInfo.classList.add('book__info');
 
     bookTitle = document.createElement('div');
-    bookTitle.classList.add('title')
+    bookTitle.classList.add('book__title');
     bookTitle.textContent = (book['title']);
-    bookContainer.appendChild(bookTitle);
+    bookInfo.appendChild(bookTitle);
 
     bookAuthor = document.createElement('div');
-    bookAuthor.classList.add('author')
+    bookAuthor.classList.add('book__author');
     bookAuthor.textContent = (book['author']);
-    bookContainer.appendChild(bookAuthor);
+    bookInfo.appendChild(bookAuthor);
 
     bookPages = document.createElement('div');
-    bookPages.classList.add('pages')
+    bookPages.classList.add('book__pages');
     bookPages.textContent = (`${book['pages']} pages`);
-    bookContainer.appendChild(bookPages);
+    bookInfo.appendChild(bookPages);
 
     bookRead = document.createElement('div');
-    bookRead.classList.add('read')
-    bookRead.textContent = (book['read']);
-    bookContainer.appendChild(bookRead);
+    bookRead.classList.add('book__read');
+    readStr = (book['read']) ? 'Read' : 'Not read';
+    bookRead.textContent = (readStr);
+    bookInfo.appendChild(bookRead);
 
+    bookIcons = document.createElement('div');
+    bookIcons.classList.add('book__icons');
+
+    readIcon = document.createElement('i');
+    readIcon.setAttribute('class', 'fa-brands fa-readme');
+    bookIcons.appendChild(readIcon);
+
+    deleteIcon = document.createElement('i');
+    deleteIcon.setAttribute('class', 'fa-solid fa-circle-xmark');
+    bookIcons.appendChild(deleteIcon);
+
+    bookContainer.appendChild(bookInfo);
+    bookContainer.appendChild(bookIcons);
     booksContainer.appendChild(bookContainer);
   }
 }
 
-const submitButton = document.querySelector('.book-form__submit');
-submitButton.addEventListener('click', () => {
-  addBookToLibrary();
-  console.log(myLibrary);
-  resetInputs();
-  displayBooks();
-});
+// Clear display
+function clearDisplay() {
+  const books = document.querySelectorAll('.books__book');
+  books.forEach(book => book.remove());
+}
 
-
-const overlay = document.querySelector(".overlay");
-const addBook = document.querySelector(".add-book");
+// Toggle book form
 const bookForm = document.querySelector(".book-form");
+const addBookButton = document.querySelector(".add-book");
+const overlay = document.querySelector(".overlay");
 
 let formOpen = false;
 function toggleForm() {
   if (formOpen) {
     bookForm.style.transform = "scale(0)";
-    addBook.style.transform = "rotate(0)";
+    addBookButton.style.transform = "rotate(0)";
     overlay.style.opacity = 0;
     formOpen = false;
   } else {
     bookForm.style.transform = "scale(1)";
-    addBook.style.transform = "rotate(45deg)";
+    addBookButton.style.transform = "rotate(45deg)";
     overlay.style.opacity = 1;
     formOpen = true;
   }
 }
 
-addBook.addEventListener("click", toggleForm);
+// Events
+
+// Add book button
+addBookButton.addEventListener("click", toggleForm);
+
+// Submit button
+const submitButton = document.querySelector('.book-form__submit');
+submitButton.addEventListener('click', () => {
+  addBookToLibrary();
+  console.log(myLibrary);
+  resetInputs();
+  clearDisplay();
+  displayBooks();
+});
+
