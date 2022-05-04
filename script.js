ecb45aad8fdbc2d2608ad3d3d9e97dda782fef5d
Daniel Sanchez <danxschz@gmail.com>
2022-05-04 19:27:38 -0400

    Implement read status and delete book features

--- script.js
+++ script.js
@@ -19,16 +19,16 @@ const read = document.querySelector('#read');
 
 function addBookToLibrary() {
   // If any value is empty, do nothing
-  if (title.value === "" || author.value === "" || pages.value === "") return;
+  if (title.value === '' || author.value === '' || pages.value === '') return;
   newBook = new Book(title.value, author.value, pages.value, read.checked);
   myLibrary.push(newBook);
 }
 
-// Reset book-form inputs
+// Reset book form inputs
 function resetInputs() {
-  title.value = "";
-  author.value = "";
-  pages.value = "";
+  title.value = '';
+  author.value = '';
+  pages.value = '';
   read.checked = false;
 }
 
@@ -46,9 +46,13 @@ function displayBooks() {
   let readStr;
 
   let bookIcons;
+  let spanReadIcon;
   let readIcon;
   let deleteIcon;
+  let spanDeleteIcon;
 
+  let i = 0;
+  let j = 0;
   for (book of myLibrary) {
     bookContainer = document.createElement('div');
     bookContainer.classList.add('books__book');
@@ -80,13 +84,21 @@ function displayBooks() {
     bookIcons = document.createElement('div');
     bookIcons.classList.add('book__icons');
 
+    spanReadIcon = document.createElement('span');
+    spanReadIcon.classList.add('read-icon');
+    spanReadIcon.setAttribute('data-index', i++);
     readIcon = document.createElement('i');
     readIcon.setAttribute('class', 'fa-brands fa-readme');
-    bookIcons.appendChild(readIcon);
+    spanReadIcon.appendChild(readIcon);
+    bookIcons.appendChild(spanReadIcon);
 
+    spanDeleteIcon = document.createElement('span');
+    spanDeleteIcon.classList.add('delete-icon');
+    spanDeleteIcon.setAttribute('data-index', j++);
     deleteIcon = document.createElement('i');
     deleteIcon.setAttribute('class', 'fa-solid fa-circle-xmark');
-    bookIcons.appendChild(deleteIcon);
+    spanDeleteIcon.appendChild(deleteIcon);
+    bookIcons.appendChild(spanDeleteIcon);
 
     bookContainer.appendChild(bookInfo);
     bookContainer.appendChild(bookIcons);
@@ -101,37 +113,80 @@ function clearDisplay() {
 }
 
 // Toggle book form
-const bookForm = document.querySelector(".book-form");
-const addBookButton = document.querySelector(".add-book");
-const overlay = document.querySelector(".overlay");
+const bookForm = document.querySelector('.book-form');
+const addBookButton = document.querySelector('.add-book');
+const overlay = document.querySelector('.overlay');
 
 let formOpen = false;
 function toggleForm() {
   if (formOpen) {
-    bookForm.style.transform = "scale(0)";
-    addBookButton.style.transform = "rotate(0)";
+    bookForm.style.transform = 'scale(0)';
+    addBookButton.style.transform = 'rotate(0)';
     overlay.style.opacity = 0;
     formOpen = false;
   } else {
-    bookForm.style.transform = "scale(1)";
-    addBookButton.style.transform = "rotate(45deg)";
+    bookForm.style.transform = 'scale(1)';
+    addBookButton.style.transform = 'rotate(45deg)';
     overlay.style.opacity = 1;
     formOpen = true;
   }
 }
 
+// Change read status
+function changeReadStatus(indexNumber) {
+  if (myLibrary[indexNumber]['read']) {
+    myLibrary[indexNumber]['read'] = false;
+  } else {
+    myLibrary[indexNumber]['read'] = true;
+  }
+}
+
+function setReadStatusEvents() {
+  let readStatusButtons = document.querySelectorAll('.read-icon');
+  readStatusButtons.forEach(readStatusButton => {
+    readStatusButton.addEventListener('click', () => {
+      changeReadStatus(readStatusButton.getAttribute('data-index'));
+      clearDisplay();
+      displayBooks();
+      setReadStatusEvents();
+      setDeleteBookEvents();
+    });
+  });
+}
+
+// Delete book
+function deleteBook(indexNumber) {
+  myLibrary.splice(indexNumber, 1);
+}
+
+function setDeleteBookEvents() {
+  let deleteBookButtons = document.querySelectorAll('.delete-icon');
+  deleteBookButtons.forEach(deleteBookButton => {
+    deleteBookButton.addEventListener('click', () => {
+      deleteBook(deleteBookButton.getAttribute('data-index'));
+      clearDisplay();
+      displayBooks();
+      setReadStatusEvents();
+      setDeleteBookEvents();
+    });
+  });
+}
+
 // Events
 
 // Add book button
-addBookButton.addEventListener("click", toggleForm);
+addBookButton.addEventListener('click', toggleForm);
 
 // Submit button
 const submitButton = document.querySelector('.book-form__submit');
 submitButton.addEventListener('click', () => {
   addBookToLibrary();
-  console.log(myLibrary);
   resetInputs();
   clearDisplay();
   displayBooks();
+  setReadStatusEvents();
 });
+
+
+
 
